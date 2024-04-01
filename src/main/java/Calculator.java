304f29de017cbcc1362c6c84456ada6e10969cfa
Владислав Захаров <vlad.zaharov.1718@gmail.com>
2024-04-01 19:22:30 +0400

    Pract work №1

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Calculator {
+    int people;
+    float total = 0;
+    ArrayList<Product> products = new ArrayList<>();
+    Calculator(int people){
+        this.people = people;
+    }
+    public void addProduct(Product newProduct){
+        products.add(newProduct);
+        total += newProduct.price;
+    }
+    public void print(){
+        System.out.println("Добавленные товары:");
+        for (int i = 0; i < products.size(); i ++){
+            System.out.println(String.format("Продукт %s с ценой в %.2f %s", products.get(i).name, products.get(i).price, rubleFormat(products.get(i).price)));
+        }
+        System.out.println(String.format("\nИтого: %.2f %s", total, rubleFormat(total)));
+        float perPerson = total / (float) people;
+        System.out.println(String.format("Получается %.2f %s с человека", perPerson, rubleFormat(perPerson)));
+    }
+
+    public String rubleFormat(float price){
+        int check = (int) price;
+        String result = "";
+        if (check % 10 == 1){
+            result = "рубль";
+        } else if (check % 10 >= 2 && check % 10 <= 4) {
+            result = "рубля";
+        }
+        else {
+            result = "рублей";
+        }
+        return result;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,44 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator calculator;
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("На скольких человек необходимо разделить счёт?");
+            int peopleCount = scanner.nextInt();
+            scanner.nextLine();
+            if (peopleCount == 1){
+                System.out.println("Зачем что то считать для одного человека?) Повторите попытку");
+            }
+            else if (peopleCount < 0) {
+                System.out.println("Некорректное значение для подсчёта. Повторите попытку");
+            }
+            else if (peopleCount > 1){
+                calculator = new Calculator(peopleCount);
+                System.out.println("Ок, начинаем считать!");
+                break;
+            }
+        }
+
+        String name;
+        float price;
+        while (true){
+            System.out.println(
+                    "Добавим товар в список! \n" +
+                    "Введите название товара:");
+            name = scanner.nextLine();
+            System.out.println("Введите цену товара в формате [рубли],[копейки], например 10,45 или 11,40:");
+            price = scanner.nextFloat();
+            scanner.nextLine();
+            calculator.addProduct(new Product(name, price));
+            System.out.println(String.format("Товар %s с ценой %.2f добавлен!", name, price));
+            System.out.println("Если вы хотите завершить процесс добавления товаров, введите 'Завершить'");
+            if (scanner.nextLine().equalsIgnoreCase("Завершить")){
+                break;
+            }
+        }
+
+        calculator.print();
     }
 }
